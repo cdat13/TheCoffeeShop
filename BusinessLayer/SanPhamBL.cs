ce1da9ea3dbffba9b022a12586eb8ca67b3ffab4
rygjpsuo <ctphuc7247@gmail.com>
2025-05-14 01:37:59 +0700

    Add files via upload

---
+++ BusinessLayer/SanPhamBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BusinessLayer;
+using DataLayer;
+using DataTranfer;
+
+namespace BusinessLayer
+{
+    public class SanPhamBL
+    {
+        private SanPhamDL dal = new SanPhamDL();
+
+        public List<SanPham> GetAll() => dal.GetSanPhams();
+        public void Add(SanPham p) => dal.ThemSanPham(p);
+        public void Update(SanPham p) => dal.CapNhatSanPham(p);
+        public void Delete(int id) => dal.XoaSanPham(id);
+    }
+
+}
