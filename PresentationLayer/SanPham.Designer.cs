c6a52d57e1e40c4d37fb758f329f628929bed169
rygjpsuo <ctphuc7247@gmail.com>
2025-05-14 02:06:07 +0700

    Add files via upload

---
+++ PresentationLayer/SanPham.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PresentationLayer
+{
+    partial class SanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvsanpham = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbname = new System.Windows.Forms.TextBox();
+            this.txbprice = new System.Windows.Forms.TextBox();
+            this.txbcategory = new System.Windows.Forms.TextBox();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvsanpham)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvsanpham
+            // 
+            this.dgvsanpham.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvsanpham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvsanpham.Location = new System.Drawing.Point(-2, 288);
+            this.dgvsanpham.Name = "dgvsanpham";
+            this.dgvsanpham.ReadOnly = true;
+            this.dgvsanpham.RowHeadersWidth = 51;
+            this.dgvsanpham.RowTemplate.Height = 24;
+            this.dgvsanpham.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvsanpham.Size = new System.Drawing.Size(805, 150);
+            this.dgvsanpham.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên Sản Phẩm:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GIá:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Loại:";
+            // 
+            // txbname
+            // 
+            this.txbname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbname.Location = new System.Drawing.Point(182, 53);
+            this.txbname.Name = "txbname";
+            this.txbname.Size = new System.Drawing.Size(305, 30);
+            this.txbname.TabIndex = 4;
+            // 
+            // txbprice
+            // 
+            this.txbprice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbprice.Location = new System.Drawing.Point(182, 109);
+            this.txbprice.Name = "txbprice";
+            this.txbprice.Size = new System.Drawing.Size(305, 30);
+            this.txbprice.TabIndex = 5;
+            // 
+            // txbcategory
+            // 
+            this.txbcategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbcategory.Location = new System.Drawing.Point(182, 164);
+            this.txbcategory.Name = "txbcategory";
+            this.txbcategory.Size = new System.Drawing.Size(305, 30);
+            this.txbcategory.TabIndex = 6;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnthem.Location = new System.Drawing.Point(594, 60);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(120, 43);
+            this.btnthem.TabIndex = 7;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsua.Location = new System.Drawing.Point(594, 130);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(120, 43);
+            this.btnsua.TabIndex = 8;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnxoa.Location = new System.Drawing.Point(594, 203);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(120, 43);
+            this.btnxoa.TabIndex = 9;
+            this.btnxoa.Text = "Xoá";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // SanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txbcategory);
+            this.Controls.Add(this.txbprice);
+            this.Controls.Add(this.txbname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvsanpham);
+            this.Name = "SanPham";
+            this.Text = "SanPham";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvsanpham)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvsanpham;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbname;
+        private System.Windows.Forms.TextBox txbprice;
+        private System.Windows.Forms.TextBox txbcategory;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+    }
+}
---
+++ PresentationLayer/SanPham.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using BusinessLayer;
+using DataTranfer;
+
+namespace PresentationLayer
+{
+    public partial class SanPham : Form
+    {
+        private SanPhamBL bl = new SanPhamBL();
+
+        public SanPham()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            dgvsanpham.DataSource = bl.GetAll();
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            var p = new DataTranfer.SanPham
+            {
+                Name = txbname.Text,
+                Price = double.Parse(txbprice.Text),
+                Category = txbcategory.Text
+            };
+            bl.Add(p);
+            LoadData();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            if (dgvsanpham.CurrentRow != null)
+            {
+                var p = new DataTranfer.SanPham
+                {
+                    Id = (int)dgvsanpham.CurrentRow.Cells["Id"].Value,
+                    Name = txbname.Text,
+                    Price = double.Parse(txbprice.Text),
+                    Category = txbcategory.Text
+                };
+                bl.Update(p);
+                LoadData();
+            }
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            if (dgvsanpham.CurrentRow != null)
+            {
+                int id = (int)dgvsanpham.CurrentRow.Cells["Id"].Value;
+                var confirm = MessageBox.Show("Bạn có chắc chắn muốn xoá sản phẩm này?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (confirm == DialogResult.Yes)
+                {
+                    bl.Delete(id);
+                    LoadData();
+                }
+            }
+        }
+
+        private void dgvsanpham_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvsanpham.CurrentRow != null)
+            {
+                txbname.Text = dgvsanpham.CurrentRow.Cells["Name"].Value.ToString();
+                txbprice.Text = dgvsanpham.CurrentRow.Cells["Price"].Value.ToString();
+                txbcategory.Text = dgvsanpham.CurrentRow.Cells["Category"].Value.ToString();
+            }
+        }
+    }
+}
